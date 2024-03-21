c5806d1d73ff040591901f277206bcd554c3e9c2
atomwoz <atwozniak67@gmail.com>
2024-03-21 11:43:45 +0100

    Styled products

--- src/views/PromotionView.vue
+++ src/views/PromotionView.vue
@@ -1,10 +1,17 @@
 <template>
     <div>
         <Header></Header>
-        <h2>{{ promotion.header }}</h2>
-        <h3>{{ promotion.description }}</h3>
-        <p>{{ promotion.longDescription }}</p>
-        <ProductTile v-for="product in promotion.items" :key="product" :productID="product" />
+        <div class="app">
+            <div class="header">
+                <h2>{{ promotion.header }}</h2>
+                <h3>{{ promotion.description }}</h3>
+                <p>{{ promotion.longDescription }}</p>
+            </div>
+            <div class="products">
+                <ProductTile class="product-tile" v-for="product in promotion.items" :key="product"
+                    :productID="product" />
+            </div>
+        </div>
         <Footer></Footer>
     </div>
 </template>
@@ -35,4 +42,72 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+* {
+    box-sizing: border-box;
+    overflow: hidden;
+}
+
+.app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 20px;
+    width: 100vw;
+}
+
+.header {
+    text-align: center;
+    margin-bottom: 20px;
+    width: 500px;
+}
+
+.products {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+
+.product-tile {
+    background-color: #f5f5f5;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    width: 250px;
+    height: 250px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 10px;
+}
+
+.product-tile h4 {
+    margin-top: 0;
+    margin-bottom: 10px;
+}
+
+.product-tile p {
+    margin-top: 0;
+    margin-bottom: 20px;
+}
+
+.product-tile button {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+.product-tile button:hover {
+    background-color: #0056b3;
+}
+
+.product-tile button:focus {
+    outline: none;
+}
+</style>
